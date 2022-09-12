986c6482f7c1f92a5ee6200d09c4cf924f5d84f1
CayoHenrique250 <chsf2@aluno.ifal.edu.br>
2022-09-12 17:21:36 +0000

    divisao_atualizada

--- divisao.java
+++ divisao.java
@@ -9,9 +9,9 @@ public class divisao {
     @GET
     public double calcula(@PathParam("nf") String fStr, @PathParam("ns") String sStr) {
         try {
-            double n2 = Double.parseDouble(fStr);
+            double n1 = Double.parseDouble(fStr);
             double n2 = Double.parseDouble(sStr);
-            double result = n2 / n2;
+            double result = n1 / n2;
             return result;
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Operador inválido.");
