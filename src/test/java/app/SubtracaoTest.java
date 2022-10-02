187f87eb81d368ebaf9e7499a0af297ef6b5ae56
CayoHenrique250 <chsf2@aluno.ifal.edu.br>
2022-10-02 02:46:26 +0000

    subtracao_test

---
+++ src/test/java/app/SubtracaoTest.java
@@ -0,0 +1,28 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SubtracaoTest {
+
+    Subtracao subtracao;
+
+    @BeforeEach
+    public void before() {
+        subtracao = new Subtracao();
+    }
+
+    @Test
+    public void verificarCasoBase(){
+        double resultado = subtracao.calcula("4","3");
+        assertEquals(1.0, resultado);
+    }
+
+    @Test
+    public void verificarSubtracaoDouble(){
+        double resultado = subtracao.calcula("6.5", "2.7");
+        assertEquals(4.8, resultado);
+    }
+
+}
