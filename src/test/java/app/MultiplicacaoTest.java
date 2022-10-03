f9d1ab52cf0b12d188b217d76bf6fdb3fc6de0c3
oagarian <102990211+oagarian@users.noreply.github.com>
2022-10-03 08:12:13 -0300

    Create MultiplicacaoTest.java

---
+++ src/test/java/app/MultiplicacaoTest.java
@@ -0,0 +1,35 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
+public class MultiplicacaoTest {
+    
+    Multiplicacao multiplicacao;
+    @BeforeEach
+    public void before() {
+        multiplicacao = new Multiplicacao();
+    }
+
+
+    @Test
+    public void verificaCasoBase() {
+        double resultado = multiplicacao.calcula("10", "20");
+        assertEquals(200, resultado);
+    }
+
+    @Test
+    public void verificaMultiplicacaoDecimal() {
+        double resultado = multiplicacao.calcula("422.23", "3");
+        assertEquals(1266.69, resultado, 0.0001);
+    }
+    
+    @Test
+    public void verificaMultiplicacaoZero() {
+        double resultado = multiplicacao.calcula("-2424", "-0");
+        assertEquals(0, resultado);
+    }
+
+
+}
